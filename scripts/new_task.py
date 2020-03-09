42717a8065ff6a364e69d926564b52783624674a
Lukas Šupienis <lukas.supienis@adform.com>
2020-03-09 23:55:19 +0200

    durable messages

--- scripts/new_task.py
+++ scripts/new_task.py
@@ -21,8 +21,13 @@ if __name__ == "__main__":
         )
     )
     channel = connection.channel()
-    channel.queue_declare(queue="hello", durable=True)
+    channel.queue_declare(queue="task_queue", durable=True)
     message = " ".join(sys.argv[1:]) or "Hello World!"
-    channel.basic_publish(exchange="", routing_key="hello", body=message)
+    channel.basic_publish(
+        exchange="",
+        routing_key="task_queue",
+        body=message,
+        properties=pika.BasicProperties(delivery_mode=2),
+    )
     print(" [x] Sent %r" % message)
     connection.close()
